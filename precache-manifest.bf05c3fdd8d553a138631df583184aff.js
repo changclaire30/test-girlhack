adcca6b966d3f5120f64f5d4a6029faa10266f18
Claire Chang <c89650508@hotmail.com>
2020-10-11 18:23:17 -0500

    Updates

---
+++ precache-manifest.bf05c3fdd8d553a138631df583184aff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e189a5e8b5235ae8c8a6d1582c1e7ecb",
+    "url": "/test-girlhack/index.html"
+  },
+  {
+    "revision": "91eea4443ce9198c09fe",
+    "url": "/test-girlhack/static/css/main.bccb7007.chunk.css"
+  },
+  {
+    "revision": "b58c2cc9a7e1f1895e91",
+    "url": "/test-girlhack/static/js/2.7bd428ab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test-girlhack/static/js/2.7bd428ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91eea4443ce9198c09fe",
+    "url": "/test-girlhack/static/js/main.3837c2f1.chunk.js"
+  },
+  {
+    "revision": "4d8ead44637b4db6240b",
+    "url": "/test-girlhack/static/js/runtime-main.4b864778.js"
+  }
+]);
